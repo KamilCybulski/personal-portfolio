3d98c9de6a64ff39afd0c411ae444eb174dc8a1a
Kamyl <kamillcybulski@gmail.com>
2017-12-13 14:43:42 +0100

    Refactor caption overlays and transformations

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const addProjectOnHoverHandlers = () => {
   const addOnHoverListener = (elem) => {
     const child = elem.lastElementChild;
+    const link = elem.previousElementSibling;
 
     elem.addEventListener('mouseenter', () => {
+      link.classList.add('project__link_hidden');
       elem.classList.add('project__caption_visible');
       child.classList.add('project__description_visible');
     });
 
     elem.addEventListener('mouseleave', () => {
+      link.classList.remove('project__link_hidden');
       elem.classList.remove('project__caption_visible');
       child.classList.remove('project__description_visible');
     });
